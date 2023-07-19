adfef49d6b7063ac6fcff392d1f3827a7ac04bd5
Aubin Sionville <aubinsionville@gmail.com>
2023-07-19 20:08:29 +0200

    Added randoms

--- includes/utils.c
+++ includes/utils.c
@@ -2,12 +2,36 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <math.h>
+#include "vector.h"
 
-double randomDouble() {
+#define PI 3.14159265359
+
+double random() {
     return (double)rand() / (double)RAND_MAX;
 }
 
-double randomDoubleRange(double min, double max) {
-    return min + (max - min) * randomDouble();
+double randomRange(double min, double max) {
+    return min + (max - min) * random();
 }
 
+double clamp(double x, double min, double max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
+}
+
+Vector randomVector() {
+    return newVector(random(), random(), random());
+}
+
+Vector randomVectorRange(double min, double max) {
+    return newVector(randomRange(min, max), randomRange(min, max), randomRange(min, max));
+}
+
+Vector randomInUnitSphere() {
+    while (true) {
+        Vector p = randomVectorRange(-1, 1);
+        if (lengthSquaredVector(p) >= 1) continue;
+        return p;
+    }
+}
--- includes/utils.h
+++ includes/utils.h
@@ -3,7 +3,10 @@
 
 #define EPSILON 0.000001
 
-double randomDouble();
-double randomDoubleRange(double min, double max);
+double randomRange(double min, double max);
+double clamp(double x, double min, double max);
+
+Vector randomVector();
+Vector randomVectorRange(double min, double max);
 
 #endif // !UTILS_H
