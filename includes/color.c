208bc834524ea220dd71d991c46060af5579f03b
Aubin Sionville <57773476+ASionville@users.noreply.github.com>
2023-07-19 15:09:33 +0200

    Added colors

---
+++ includes/color.c
@@ -0,0 +1,68 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include "color.h"
+
+// Create a new color
+Color newColor(double r, double g, double b) {
+    Color c;
+    c.r = r;
+    c.g = g;
+    c.b = b;
+    return c;
+}
+
+// Check if two colors are equal
+bool equalColor(Color c1, Color c2) {
+    return c1.r == c2.r && c1.g == c2.g && c1.b == c2.b;
+}
+
+// Add two colors
+Color addColor(Color c1, Color c2) {
+    Color c;
+    c.r = c1.r + c2.r;
+    c.g = c1.g + c2.g;
+    c.b = c1.b + c2.b;
+    return c;
+}
+
+// Subtract two colors
+Color subtractColor(Color c1, Color c2) {
+    Color c;
+    c.r = c1.r - c2.r;
+    c.g = c1.g - c2.g;
+    c.b = c1.b - c2.b;
+    return c;
+}
+
+// Multiply two colors
+Color mulColor(Color c1, Color c2) {
+    Color c;
+    c.r = c1.r * c2.r;
+    c.g = c1.g * c2.g;
+    c.b = c1.b * c2.b;
+    return c;
+}
+
+// Multiply a color by a scalar
+Color scaleColor(Color c, double s) {
+    Color c2;
+    c2.r = c.r * s;
+    c2.g = c.g * s;
+    c2.b = c.b * s;
+    return c2;
+}
+
+// Lerp between two colors
+Color lerpColor(Color c1, Color c2, double t) {
+    Color c;
+    c.r = c1.r + (c2.r - c1.r) * t;
+    c.g = c1.g + (c2.g - c1.g) * t;
+    c.b = c1.b + (c2.b - c1.b) * t;
+    return c;
+}
+
+// Print a color
+void printColor(Color c) {
+    printf("(%f, %f, %f)\n", c.r, c.g, c.b);
+}
---
+++ includes/color.h
@@ -0,0 +1,24 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+typedef struct {
+    double r;
+    double g;
+    double b;
+} Color;
+
+Color newColor(double r, double g, double b);
+
+bool equalColor(Color c1, Color c2);
+
+Color addColor(Color c1, Color c2);
+Color subColor(Color c1, Color c2);
+
+Color mulColor(Color c1, Color c2);
+
+Color scaleColor(Color c, double s);
+Color lerpColor(Color c1, Color c2, double t);
+
+void printColor(Color c);
+
+#endif // !COLOR_H
